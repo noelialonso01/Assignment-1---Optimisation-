e38dae24cce3de6955fdc29e49f29e677dc15721
alex-schonrock <alex@schonrocks.com>
2025-09-27 11:27:45 +0200

    first trial :)

--- src/data_ops/data_loader.py
+++ src/data_ops/data_loader.py
@@ -14,6 +14,9 @@ import xarray as xr
 import numpy as np
 import yaml
 
+from src.utils.utils import load_dataset
+
+
 
 class DataLoader:
     """
@@ -29,7 +32,11 @@ class DataLoader:
     question: str
     input_path: Path
 
-    def __init__(self):
+    def __init__(self, input_path: str, question: str = "question_1a"):
+        ### These self definitions are establishing the class attributes, not used yet
+        self.input_path = Path(self.input_path).resolve()
+        self.load_aux_data('question1a_scenario1_aux_data.yaml')
+        self.data()
         """
         Post-initialization to load and validate all required datasets (placeholder function)
 
@@ -45,12 +52,18 @@ class DataLoader:
         pass
 
     def _load_dataset(self, question_name: str):
+        self.question = question_name
+        raw_data = load_dataset(question_name)
+        for key, value in raw_data.items():
+            setattr(self, key, pd.DataFrame(value))
+
         """Helper function to load all CSV or json files, using the appropriate method based on file extension.
         
         example usage: 
         call the load_dataset() function from utils.py to load all files in the input_path directory
         save all data as class attributes (e.g. self.demand, self.wind, etc.), structured as pandas DataFrames or Series (or other format as prefered)
         """
+
         pass
 
 
@@ -74,3 +87,15 @@ class DataLoader:
         Attach key values as class attributes (flattened).
         """
         pass
+
+
+"""
+When running, define path and question as inputs. Path should be:
+
+"""
+
+path = "C:\Users\alex\OneDrive\Desktop\DTU\Optimistation\Assignment-1---Optimisation"
+question = "question_1a"
+data = DataLoader(input_path=path, question=question)
+loaded_data = data._load_dataset(question)
+print(loaded_data)
